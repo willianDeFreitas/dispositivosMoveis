9e50ed9982b899747b7f35ed98c3677ae91deb07
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2019-08-08 21:08:24 -0300

    android layout training 'Hello Toast Challenge'

--- app/src/main/java/com/example/exeaulaandroid08_08_2019/MainActivity.java
+++ app/src/main/java/com/example/exeaulaandroid08_08_2019/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.exeaulaandroid08_08_2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+
+        toast.show();
+        exibeMCount(true);
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        exibeMCount(false);
+    }
+
+    private void exibeMCount(Boolean zera) {
+        if(zera){
+            mCount = 0;
+        }
+
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
